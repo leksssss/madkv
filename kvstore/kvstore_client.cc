04243494d37b9b601298da1f0326aca73600ab52
AviKart <AviKart@node0.group23.advosuwmadison-pg0.wisc.cloudlab.us>
2026-02-14 13:05:22 -0600

    Initial commit

---
+++ kvstore/kvstore_client.cc
@@ -0,0 +1,219 @@
+#include <grpcpp/grpcpp.h>
+
+#include <iostream>
+#include <memory>
+#include <string>
+#include <fstream>
+
+#include "absl/flags/flag.h"
+#include "absl/flags/parse.h"
+#include "cmake/build/kvstore.pb.h"
+#include "cmake/build/kvstore.grpc.pb.h"
+
+using grpc::Channel;
+using grpc::ChannelArguments;
+using grpc::ClientContext;
+using grpc::Status;
+using kvstore::Kvstore;
+using kvstore::PutRequest;
+using kvstore::PutResponse;
+using kvstore::SwapRequest;
+using kvstore::SwapResponse;
+using kvstore::GetRequest;
+using kvstore::GetResponse;
+using kvstore::ScanRequest;
+using kvstore::ScanResponse;
+using kvstore::DeleteRequest;
+using kvstore::DeleteResponse;
+using kvstore::KeyValuePair;
+using namespace std;
+
+ABSL_FLAG(string, target, "localhost:3777", "Server address");
+
+class KvstoreClient {
+ public:
+  KvstoreClient(shared_ptr<Channel> channel)
+      : stub_(Kvstore::NewStub(channel)) {}
+
+  // Assembles the client's payload, sends it and presents the response back
+  // from the server.
+  void Put(const string& key, const string& value) {
+    // Data we are sending to the server.
+    PutRequest request;
+    request.set_key(key);
+    request.set_new_value(value);
+
+    // Container for the data we expect from the server.
+    PutResponse response;
+
+    // Context for the client. It could be used to convey extra information to
+    // the server and/or tweak certain RPC behaviors.
+    ClientContext context;
+
+    // The actual RPC.
+    Status status = stub_->Put(&context, request, &response);
+
+    // Act upon its status.
+    if (status.ok()) {
+      if (response.found()) {
+        cout << "PUT " << key << " found\n";
+      } else {
+        cout << "PUT " << key << " not_found\n";
+      }
+    } else {
+      cout << status.error_code() << ": " << status.error_message() << endl;
+    }
+  }
+
+  void Swap(const string& key, const string& new_value) {
+    SwapRequest request;
+    request.set_key(key);
+    request.set_new_value(new_value);
+
+    SwapResponse response;
+    ClientContext context;
+
+    Status status = stub_->Swap(&context, request, &response);
+    
+    if(status.ok()) {
+      if(response.has_old_value()) {
+        cout << "SWAP " << key <<  " " << response.old_value() << endl; 
+      } else {
+       cout << "SWAP " << key <<  " null\n";  
+      }
+    } else {
+      cout << status.error_code() << ":" << status.error_message() << endl;
+    }
+  }
+
+  void Get(const string& key) {
+    GetRequest request;
+    request.set_key(key);
+    
+    GetResponse response;
+    ClientContext context;
+
+    Status status = stub_->Get(&context, request, &response);
+
+    if(status.ok()) {
+      if(response.has_value()) {
+        cout << "GET " << key <<  " " << response.value() << endl; 
+      } else {
+        cout << "GET " << key <<  " null\n"; 
+      }    
+    } else {
+      cout << status.error_code() << ":" << status.error_message() << endl;
+    }
+  }
+
+  void Scan(const string& start_key, const string& end_key) {
+    // Data we are sending to the server.
+    ScanRequest request;
+    request.set_start_key(start_key);
+    request.set_end_key(end_key);
+
+    // Container for the data we expect from the server.
+    ScanResponse response;
+
+    // Context for the client. It could be used to convey extra information to
+    // the server and/or tweak certain RPC behaviors.
+    ClientContext context;
+
+    // The actual RPC.
+    Status status = stub_->Scan(&context, request, &response);
+
+    // Act upon its status.
+    if (status.ok()) {
+      cout << "SCAN " << start_key << " " << end_key << " BEGIN\n";
+      for (const auto& pair : response.pairs()) {
+        cout << "\t" << pair.key() << " " << pair.value() << "\n";
+      }
+      cout << "SCAN END\n";
+    } else {
+      cout << status.error_code() << ": " << status.error_message() << endl;
+    }
+  }
+
+  void Delete(const string& key) {
+    DeleteRequest request;
+    request.set_key(key);
+
+    DeleteResponse response;
+    ClientContext context;
+
+    Status status = stub_->Delete(&context, request, &response);
+
+    if(status.ok()) {
+      if (response.found()) {
+        cout << "DELETE " << key << " found\n";
+      } else {
+        cout << "DELETE " << key << " not_found\n";
+      }
+    } else {
+      cout << status.error_code() << ": " << status.error_message() << endl;
+    }
+  }
+
+ private:
+  unique_ptr<Kvstore::Stub> stub_;
+};
+
+vector<string> split(const string& str, const string& delimiter = " ") {
+    vector<string> tokens;
+    size_t pos = 0;
+    size_t lastPos = 0;
+
+    while ((pos = str.find(delimiter, lastPos)) != string::npos) {
+        tokens.push_back(str.substr(lastPos, pos - lastPos));
+        lastPos = pos + delimiter.length();
+    }
+
+    tokens.push_back(str.substr(lastPos));
+
+    return tokens;
+}
+
+int main(int argc, char** argv) {
+  absl::ParseCommandLine(argc, argv);
+  // Instantiate the client. It requires a channel, out of which the actual RPCs
+  // are created. This channel models a connection to an endpoint specified by
+  // the argument "--target=" which is the only expected argument.
+  string target_str = absl::GetFlag(FLAGS_target);
+  // Setting channel args
+  // ChannelArguments channel_args;
+  // channel_args.SetInt("grpc.tcp_nodelay", 1);
+  // channel_args.SetInt(GRPC_ARG_KEEPALIVE_TIME_MS, 20000);
+  // channel_args.SetInt(GRPC_ARG_KEEPALIVE_TIMEOUT_MS, 10000);
+  // We indicate that the channel isn't authenticated (use of
+  // InsecureChannelCredentials()).
+  KvstoreClient kvstore(
+      grpc::CreateChannel(target_str, grpc::InsecureChannelCredentials()));
+
+  while (true) {
+    string input;
+    getline(cin, input);
+
+    if (input == "STOP") { // stop reading stdin, exit
+      cout << "STOP\n";
+      break;
+    }
+
+    vector<string> query = split(input);
+    if (query[0] == "PUT") {
+      kvstore.Put(query[1], query[2]);
+    } else if (query[0] == "SWAP"){
+      kvstore.Swap(query[1], query[2]);
+    } else if (query[0] == "GET"){
+      kvstore.Get(query[1]);
+    } else if (query[0] == "SCAN"){
+      kvstore.Scan(query[1], query[2]);
+    } else if (query[0] == "DELETE"){
+      kvstore.Delete(query[1]);
+    } else {
+      cout << "Invalid input: " << input << endl;
+      return 1;
+    }
+  }
+
+  return 0;
+}
---
+++ kvstore/kvstore_server.cc
@@ -0,0 +1,129 @@
+#include <grpcpp/ext/proto_server_reflection_plugin.h>
+#include <grpcpp/grpcpp.h>
+#include <grpcpp/health_check_service_interface.h>
+
+#include <iostream>
+#include <memory>
+#include <string>
+#include "absl/log/initialize.h"
+#include "absl/flags/flag.h"
+#include "absl/flags/parse.h"
+#include "cmake/build/kvstore.pb.h"
+#include "cmake/build/kvstore.grpc.pb.h"
+
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::Status;
+using kvstore::Kvstore;
+using kvstore::PutRequest;
+using kvstore::PutResponse;
+using kvstore::SwapRequest;
+using kvstore::SwapResponse;
+using kvstore::GetRequest;
+using kvstore::GetResponse;
+using kvstore::ScanRequest;
+using kvstore::ScanResponse;
+using kvstore::DeleteRequest;
+using kvstore::DeleteResponse;
+using kvstore::KeyValuePair;
+using namespace std;
+
+ABSL_FLAG(uint16_t, port, 3777, "Server port for the service");
+
+class KvstoreServiceImpl final : public Kvstore::Service {
+  private:
+    map<string, string> db;
+
+  public:
+    Status Put(ServerContext* context, const PutRequest* request,
+                    PutResponse* response) override {
+      auto it = db.find(request->key());
+      if (it != db.end()) {
+        response->set_found(true);
+        it->second = request->new_value();
+      } else {
+        db[request->key()] = request->new_value();
+      }
+
+      return Status::OK;
+    }
+
+    Status Swap(ServerContext* context, const SwapRequest* request,
+                    SwapResponse* response) override {
+      auto it = db.find(request->key());
+      if (it != db.end()) {
+        response->set_old_value(it->second);
+        it->second = request->new_value();
+      } else {
+        db[request->key()] = request->new_value();
+      }
+      
+      return Status::OK;
+    }
+
+    Status Get(ServerContext* context, const GetRequest* request,
+                    GetResponse* response) override {
+      auto it = db.find(request->key());
+      if(it != db.end()) {
+        response->set_value(it->second);
+      }
+
+      return Status::OK;
+    }
+
+    Status Scan(ServerContext* context, const ScanRequest* request,
+                    ScanResponse* response) override {
+      auto it = db.lower_bound(request->start_key());
+      while (it != db.end() && it->first <= request->end_key()) {
+        auto* pair = response->add_pairs();
+        pair->set_key(it->first);
+        pair->set_value(it->second);
+        it++;
+      }
+
+      return Status::OK;
+    }
+
+    Status Delete(ServerContext* context, const DeleteRequest* request,
+                    DeleteResponse* response) override {
+      size_t count = db.erase(request->key());
+      response->set_found(count > 0);
+
+      return Status::OK;
+    }
+};
+
+void RunServer(uint16_t port) {
+  string server_address = absl::StrFormat("0.0.0.0:%d", port);
+  KvstoreServiceImpl service;
+
+  grpc::EnableDefaultHealthCheckService(true);
+  grpc::reflection::InitProtoReflectionServerBuilderPlugin();
+  ServerBuilder builder;
+  // Listen on the given address without any authentication mechanism.
+  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+  // To keep the channels always hot
+  builder.AddChannelArgument(GRPC_ARG_KEEPALIVE_TIME_MS, 20000);
+  builder.AddChannelArgument(GRPC_ARG_KEEPALIVE_PERMIT_WITHOUT_CALLS, 1);
+  builder.AddChannelArgument(GRPC_ARG_KEEPALIVE_TIMEOUT_MS, 10000);
+  builder.AddChannelArgument("grpc.internal.yield_before_poll", 0);
+  builder.AddChannelArgument("grpc.tcp_nodelay", 1);
+  // Register "service" as the instance through which we'll communicate with
+  // clients. In this case it corresponds to an *synchronous* service.
+  builder.RegisterService(&service);
+  // Finally assemble the server.
+  unique_ptr<Server> server(builder.BuildAndStart());
+  cout << "Server listening on " << server_address << std::endl;
+
+  // Wait for the server to shutdown. Note that some other thread must be
+  // responsible for shutting down the server for this call to ever return.
+  server->Wait();
+}
+
+int main(int argc, char** argv) {
+  absl::ParseCommandLine(argc, argv);
+  absl::InitializeLog();
+  RunServer(absl::GetFlag(FLAGS_port));
+  return 0;
+}
